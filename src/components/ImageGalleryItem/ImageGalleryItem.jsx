27b840b3a7ff603bcc4b0d773388a28944d53d08
Rew <irinavakulenko2000@gmail.com>
2023-06-02 18:59:25 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,30 @@
 import { LiStyled } from '../styled'
 import { Modal } from '../Modal/Modal'
 import PropTypes from "prop-types";
-import {Item} from '../styled'
+import { Item } from '../styled'
 import { useState } from 'react';
 
-export const ImageGalleryItem = ({image}) => {
+export const ImageGalleryItem = ({ image }) => {
   const [showModal, setShowModal] = useState(false);
 
-   const toggleModal = () => {
-     setShowModal(!showModal)
-   }
-
-
-    const { id, webformatURL, largeImageURL } = image;
-
-    return (
-      <>
-        {showModal && (<Modal onCloseModal={toggleModal}>
-        <Item src={largeImageURL} alt=""></Item>
-        </Modal>)}
-
-        <LiStyled onClick={toggleModal} key={id} className="gallery-item">
-        <img src={webformatURL} alt="" />
-        </LiStyled>
-        </>
-    )
+  const toggleModal = () => {
+    setShowModal(!showModal)
   }
+
+  const { id, webformatURL, largeImageURL } = image;
+
+  return (
+    <>
+      {showModal && (<Modal onCloseModal={toggleModal}>
+        <Item src={largeImageURL} alt=""></Item>
+      </Modal>)}
+
+      <LiStyled onClick={toggleModal} key={id} className="gallery-item">
+        <img src={webformatURL} alt="" />
+      </LiStyled>
+    </>
+  )
+}
 
 
 ImageGalleryItem.propTypes = {
